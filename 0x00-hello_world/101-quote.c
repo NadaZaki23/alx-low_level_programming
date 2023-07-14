e5ba0965fafa26b88fe7d8f18fd0ae9060ff5df7
Nada Zaki <nada02zaki@gmail.com>
2023-07-13 19:30:33 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <unistd.h>
+
 
 /**
  * main - Entry point
@@ -8,7 +10,6 @@
 
 int main(void)
 {
-	char str[] = "and that piece of art is useful\" - Dora Kopar 2015-10-19\n";
-	fprintf(stderr, str);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (0);
 }
