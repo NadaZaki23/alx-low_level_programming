a618620eca7b9aa60f51fdd031e435327d297e32
Nada Zaki <nada02zaki@gmail.com>
2023-07-13 18:06:03 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+ *
+ * main - Entry point
+ *
+ * prints string using puts
+ *
+ * /
+
+int main(void)
+{
+	char str[] = "\"Programming is like building a multilingual puzzle";
+	puts(str);
+
+	return(0);
+}
