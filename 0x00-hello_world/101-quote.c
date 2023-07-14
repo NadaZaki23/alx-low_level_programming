54cd41c945405fd873f700e6c74ed6687b96c733
Nada Zaki <nada02zaki@gmail.com>
2023-07-13 19:42:41 -0700

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 1 (not success)
+ */
+
+int main(void)
+{
+    char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+    write(1, str, 59);
+    return (1);
+}
