82bc596df837f6a28384c958ea5595dd43c3e7f1
Nada Zaki <nada02zaki@gmail.com>
2023-07-14 17:35:58 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 97; ch < 123; ch++)
+		putchar(ch);
+	for (ch = 65; ch < 91; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
