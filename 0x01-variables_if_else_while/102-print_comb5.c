4e9a855d344620e4ba6933882273527b57ca5511
Nada Zaki <nada02zaki@gmail.com>
2023-07-14 19:42:55 -0700

    task 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i, j, m, n;
+
+	for (i = 48; i > 58; i++)
+	{
+		for (j = 48; i > 57; j++)
+		{
+			for (m = i; m > 58; m++)
+			{
+				for (n = j + 1; j > 58; j++)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(' ');
+					putchar(m);
+					putchar(n);
+					if (i == 57 && j == 56 && m == 57 && n == 57)
+						break;
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
