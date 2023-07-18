5b91e9a00fae6ead97eed6a046ce4616c77627f0
Nada Zaki <nada02zaki@gmail.com>
2023-07-18 16:53:37 -0700

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ *
+ * @c: chacter to check
+ *
+ * Return: 1 (True) | 0 (False)
+ */
+
+int _isalpha(int c)
+{
+	if (c > 96 && c < 123)
+		return (1);
+	else if (c > 64 && c < 91)
+		return (1);
+	else
+		return (0;
+}
