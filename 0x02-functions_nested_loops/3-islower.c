db861e64fc1ae752b0e2e99f8ac379b33e858533
Nada Zaki <nada02zaki@gmail.com>
2023-07-18 16:41:24 -0700

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase character
+ *
+ * Return: 1 (True)
+ *
+ * Return: 0 (False)
+ */
+
+int islower(int c)
+{
+	if (c > 96 && c < 123)
+		return (1);
+	else
+		return (0);
+}
