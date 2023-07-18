0325a27b2d6ad393142bbe2a609aeb67ad87c9f7
Nada Zaki <nada02zaki@gmail.com>
2023-07-18 09:26:24 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
+#include <main.h>
 
 /**
  * main - Entry point
- *
+ * 
  * Return: Always 0 (Success)
  */
 
-int main(void)
+int _putchar (void)
 {
 	printf("_putchar\n");
+}
+
+int main (void)
+{
+	_putchar();
 	return (0);
 }
