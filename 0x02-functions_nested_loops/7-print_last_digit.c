62061660aa6cc8154f8195971ffaf97cc56b4f91
Nada Zaki <nada02zaki@gmail.com>
2023-07-18 17:27:34 -0700

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,18 +3,14 @@
 /**
  * print_last_digit - prints last digit of a number
  * @n: given number to function
- * @m: value of last digit
  *
  * Return: value of the last digit
  */
 
 int print_last_digit(int n)
 {
-	int m;
-
 	if (n < 0)
 		n = n * -1;
-	m = n % 10;
-	_putchar('0' + m);
-	return (m);
+	_putchar('0' + (n % 10));
+	return (n % 10);
 }
