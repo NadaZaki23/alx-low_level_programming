4806e9d2d67c2dc35d04d88dd842f810fc89b979
Nada Zaki <nada02zaki@gmail.com>
2023-07-18 18:11:01 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add- adds two integers
+ * @num1: first input
+ * @num2: second input
+ * Return: result
+ */
+
+int add(int num1, int num2)
+{
+	int result;
+
+	result = num1 + num2;
+	return (result);
+}
