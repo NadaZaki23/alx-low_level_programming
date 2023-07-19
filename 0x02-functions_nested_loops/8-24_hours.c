aaa86e3dd1b7bbfa92f3aa68299bcbf13c39cb80
Nada Zaki <nada02zaki@gmail.com>
2023-07-18 18:01:54 -0700

    task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute from 00:00 to 23:59
+ * @hour: count for hours
+ * @min: count for mins
+ */
+
+void jack_bauer(void)
+{
+	int hour, min;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (min = 0; min < 60; min++)
+		{
+			_putchar('0' + (hours / 10));
+			_putchar('0' + (hours % 10));
+			_putchar(':');
+			_putchar('0' + (min / 10));
+			_putchar('0' + (min % 10));
+			_putchar('\n');
+			min++;
+		}
+	}
+}
