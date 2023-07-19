89048892bb51feacdf086cf62f18c540e4a49572
Nada Zaki <nada02zaki@gmail.com>
2023-07-18 17:13:04 -0700

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit of a number
+ *
+ * @n: given number to function
+ * @m: value of last digit
+ *
+ * Return: value of the last digit
+ */
+
+int print_last_digit(int n)
+{
+	m = n % 10;
+	return (m);
+}
