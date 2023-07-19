a00c20a75a21e719b68ac813501807783b100b44
Nada Zaki <nada02zaki@gmail.com>
2023-07-18 19:38:54 -0700

    task 16

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	long f0 = 1;
+	long f1 = 1;
+	long fn;
+	int count;
+
+	for (count = 0; count < 98; count++)
+	{
+		if (count == 0)
+		{
+			printf("%ld, ", f1);
+			continue;
+		}
+		fn = f0 + f1;
+		f0 = f1;
+		f1 = fn;
+		if (count == 49)
+		{
+			printf("%ld\n", fn);
+		}
+		else
+		{
+			printf("%ld, ", fn);
+		}
+	}
+	return (0);
+}
