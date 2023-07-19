c9cc0989aa392b368c118cfd7c0d1a286d1ae9ba
Nada Zaki <nada02zaki@gmail.com>
2023-07-18 19:33:56 -0700

    task 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - print sum of even terms in fibonnaci
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	long sum;
+	long f0 = 1;
+	long f1 = 1;
+	long fn;
+
+	while (b < 4000000)
+	{
+		fn = f0 + f1;
+		f0 = f1;
+		f1 = fn;
+		if (fn <= 4000000 && fn % 2 == 0)
+			sum += fn;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
