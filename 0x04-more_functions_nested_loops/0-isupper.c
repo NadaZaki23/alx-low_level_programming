ca0ba8857dba561b4966d331040bd82d29bf85cb
Nada Zaki <nada02zaki@gmail.com>
2023-07-20 06:59:21 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#define "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * 
+ * @c: input character to be checked
+ * 
+ * Return: 1 (True) | 0 (False)
+ */
+
+int _isupper(int c)
+{
+	if (c > 64 && c < 91)
+		return (1);
+	else
+		return (0);
+}
