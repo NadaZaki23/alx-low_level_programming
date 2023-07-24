9bfe58f658d8b9907c0e9b4ad9afe4da74872494
Nada Zaki <nada02zaki@gmail.com>
2023-07-24 09:59:32 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line
+ * @s: input string
+ */
+
+void print_rev(char *s)
+{
+	int head = s;
+
+	while (1)
+	{
+		if (*s == '\0')
+			break;
+		s += 1;
+	}
+	for (s = s - 1; s >= head; s--)
+	{
+		_putchar(*s);
+	}
+	_putchar('\n');
+}
