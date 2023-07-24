1330bd1203aefeb9b1585a0c83e3f7d2de6abe90
Nada Zaki <nada02zaki@gmail.com>
2023-07-24 14:18:00 -0700

    task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,12 +10,14 @@
 char *_strcpy(char *dest, char *src)
 {
 	int i;
+	char *p = dest;
 
-	while (src[i] != '\0')
+	while (*src != '\0')
 	{
-		dest[i] = src[i];
-		i++;
+		*dest = *src;
+		*dest++;
+		src++;
 	}
-	dest[i] = '\0';
-	return (dest);
+	*dest = '\0';
+	return (p);
 }
