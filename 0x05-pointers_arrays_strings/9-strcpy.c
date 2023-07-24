978805fe597d724938a0b75b25149a9aa7d796d9
Nada Zaki <nada02zaki@gmail.com>
2023-07-24 14:43:59 -0700

    task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -4,20 +4,19 @@
  * _strcpy - copies string pointed to by src to buffer pointed to by dest
  * @dest: pointer to destination
  * @src: pointer to source
- * Return: the pointer to dest
+ * Return: the pointer to dest [pointer arithmetic]
  */
 
 char *_strcpy(char *dest, char *src)
 {
-	int i;
+	int i = 0;
 	char *p = dest;
 
-	while (*src != '\0')
+	while (*(src + i))
 	{
-		*dest = *src;
-		dest++;
-		src++;
+		*(dest + i) = *(src + i);
+		i++;
 	}
-	*dest = '\0';
+	*(dest + i) = '\0';
 	return (p);
 }
