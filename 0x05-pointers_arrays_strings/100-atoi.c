3906a7ce528b5abc6817ddfd6e2ae9b7a3adab3d
Nada Zaki <nada02zaki@gmail.com>
2023-07-24 19:51:54 -0700

    task 10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _atoi - convert a string to an integer
+ * @s: pointer to string
+ */
+
+int _atoi(char *s)
+{
+	int i, size, negative;
+	int digit = 0, number = 0;
+
+	size = strlen(s);
+	for (i = 0; i < size; i++)
+	{
+		if (s[i] == '-')
+		{
+			negative = 1;
+			continue;
+		}
+		else
+		{
+			negative = 0;
+		}
+		digit = s[i] - '0';
+		number = number * 10 + digit;
+		if (s[i + 1] < '0' || s[i + 1] > '9')
+			break;
+	}
+	if (negative == 1)
+		number = number * -1;
+	return (number);
+}
