2af3460e9fca74552b21d267aa1d5e0edfa473f1
Nada Zaki <nada02zaki@gmail.com>
2023-07-27 12:20:15 -0700

    task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: frist string
+ * @s2: second string
+ * Return: result: -1 (s1 < s2) | 0 (s1 == s2) | 1 (s1 > s2)
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i, j, result;
+
+	while (s1[i] != '\0' && s2[j] != '\0')
+	{
+		i++;
+		j++;
+	}
+
+	if (s1[i] == '\0' && s2[j] == '\0')
+	{
+		result = 0;
+	}
+	else if (s1[i] == '\0')
+	{
+		result = -1;
+	}
+	else
+	{
+		result = 1;
+	}
+	return (result);
+}
