94217933f4c8fb96084ada0e93b1bbfe013a8929
Nada Zaki <nada02zaki@gmail.com>
2023-07-27 11:56:32 -0700

    task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,7 +1,9 @@
 #include "main.h"
+/* #include <stdlib.h> */
 
 /**
  * _strncpy - cppies a string
+ * writes additionl null bytes to dest to ensure total n
  * @src: string to be copied
  * @dest: destination to be copied to
  * @n: number of bytes to be copied
@@ -12,13 +14,16 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i, j;
 
-	dest = malloc(n + 1);
+	/* dest = malloc(n + 1); */
 	i = 0;
 	j = 0;
-	while (src[j] != src[n])
+	while (src[j] != '\0')
 	{
 		dest[i++] = src[j++];
 	}
-	dest[i] = '\0';
+	for (; i <= n; i++)
+	{
+		dest[i] = '\0';
+	}
 	return (dest);
 }
