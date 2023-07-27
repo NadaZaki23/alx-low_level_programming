5c10707b1599d4fefe72c1ed0d037742764a284d
Nada Zaki <nada02zaki@gmail.com>
2023-07-27 15:10:35 -0700

    task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all word of a string
+ * @s: pointer to string
+ * Return: a pointer to the resulting string s
+ */
+
+char *cap_string(char *s)
+{
+	int i, sep = 0;
+
+	if (s[0] >= 65 && s[0] <= 90)
+		s[0] = s[0] + 32;
+
+	for (i = 1; s[i] != '\0'; i++)
+	{
+		if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' ||
+		    s[i] == ',' || s[i] == ';' || s[i] == '.' ||
+		    s[i] == '!' || s[i] == '?' || s[i] == '"' ||
+		    s[i] == '(' || s[i] == ')' || s[i] == '{' || s[i] == '}')
+		{
+			sep = 1;
+		}
+
+		if (sep == 1 && s[i] >= 65 && s[i] <= 90)
+		{
+			s[i] == s[i] + 32;
+			sep = 0;
+		}
+	}
+	return (s);
+}
