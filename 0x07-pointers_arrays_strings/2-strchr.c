001ef8aadf8cf461e427f3e67fe3f7c88ef33f88
Nada Zaki <nada02zaki@gmail.com>
2023-07-31 18:05:17 -0700

    task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
  * _strchr - locates a character in a string
@@ -12,7 +13,7 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0' || s[i] != c; i++)
+	for (i = 0; s[i] != '\0' && s[i] != c; i++)
 	{
 		;
 	}
