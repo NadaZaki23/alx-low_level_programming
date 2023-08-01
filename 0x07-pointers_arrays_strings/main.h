c0d2ca7f5bc8b17477b5c7016160533ccf975ddb
Nada Zaki <nada02zaki@gmail.com>
2023-07-31 18:54:35 -0700

    task 7

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int _putchar(char c);
+
 char *_memset(char *s, char b, unsigned int n);
 
 char *_memcpy(char *dest, char *src, unsigned int n);
@@ -10,5 +12,9 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 
 char *_strpbrk(char *s, char *accept);
+
+
+
+void print_chessboard(char (*a)[8]);
 
 #endif
