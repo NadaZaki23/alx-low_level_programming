15e3cad7aa0d755ab9c244fa7f843dc504bbfe71
Nada Zaki <nada02zaki@gmail.com>
2023-08-08 06:37:47 -0700

    everything is freed if malloc fails

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,16 +18,19 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	array = (int **)malloc(height * sizeof(int *));
-	if (array != NULL)
-	{
-		for (i = 0; i < height; i++)
-			array[i] = (int *)malloc(width * sizeof(int));
-	}
-	else
-	{
-		free (array);
+	array = malloc(height * sizeof(int *));
+	if (array == NULL)
 		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		array[i] = malloc(width * sizeof(int));
+		if (array[i] == NULL)
+		{
+			for (i = 0; i < height; i++)
+				free(grid[i]);
+			free(grid);
+			return (NULL);
+		}
 	}
 	for (i = 0; i < height; i++)
 		for (j = 0; j < width; j++)
