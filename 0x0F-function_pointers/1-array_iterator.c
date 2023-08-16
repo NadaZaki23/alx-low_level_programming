cc408b6db9c01772b0e5117e00af93c48fe2b350
Nada Zaki <nada02zaki@gmail.com>
2023-08-16 16:03:08 -0700

    task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * array_iterator - executes a function 
+ * 		  - given as a prameter on each array element
+ * @array: input array
+ * @size: array size
+ * @action: pointer to function to be executed
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (array == NULL || action == NULL)
+		return;
+	for (; size > 0; size--)
+	{
+		action(*array);
+		array++;
+	}
+}
