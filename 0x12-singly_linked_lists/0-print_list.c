23448bf9dd9b26788eb30da408c7e18fa0e9e6cd
NadaZaki23 <nada02zaki@gmail.com>
2023-08-24 17:20:37 -0700

    task 0; ;

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elemnts of a list_t list
+ * @h: head - first node of singly linked list
+ * Return: the number of nodes
+ * if str is NULL, print [0] (nil)
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
