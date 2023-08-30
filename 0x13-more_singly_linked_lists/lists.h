df802d5ab83ae30fdc1bd3e9e86f43f15ee6b72a
NadaZaki23 <nada02zaki@gmail.com>
2023-08-29 19:00:59 -0700

    task 0

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,14 @@
+#ifndef _LISTS_H_
+#define _LISTS_H_
+
+#include <stdio.h>
+
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+
+#endif
